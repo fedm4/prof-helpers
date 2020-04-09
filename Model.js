86fc1885b4ad3dfd4e68b150d1d0b60a972b5ce4
Federico Marino <fede.m.fede@gmail.com>
2020-04-09 14:37:55 -0300

    wip

--- Model.js
+++ Model.js
@@ -3,10 +3,27 @@
  * expose basic crud operations.
  */
 export default class Model{
-  constructor(model) {
+  constructor(model, {hiddenFields}) {
     this.model = model;
     this.modelData = {};
+
+    // set Hidden Fields
+    this.hiddenFields = hiddenFields || [];
   }
+
+  /**
+   * Standarized getter for modelData
+   */
+  get modelData() {
+    const ret = {};
+    for(let key in Object.keys(this.modelData)) {
+      if(this.hiddenFields.indexOf(key) === -1) {
+        ret[key] = this.modelData[key];
+      }
+    }
+    return ret;
+  }
+
   create() {
     this.model.build(this.modelData);
   }
