db7bc823abe19f53191db7e7f9463919682649e1
Federico Marino <fede.m.fede@gmail.com>
2020-04-21 10:48:00 -0300

    Fixed model

---
+++ index.js
@@ -0,0 +1,7 @@
+import Model from './src/Models/Model.js';
+import Logger from './src/Logger/Logger.js';
+
+export default {
+  Model,
+  Logger
+};
---
+++ src/Logger/Logger.js
@@ -0,0 +1,29 @@
+const initJaegerTracer = require("jaeger-client").initTracer;
+
+const initTracer = (serviceName) => {
+  const config = {
+    serviceName: serviceName,
+    sampler: {
+      type: "const",
+      param: 1,
+    },
+    reporter: {
+      logSpans: true,
+    },
+  };
+  const options = {
+    logger: {
+      info(msg) {
+        console.log("INFO ", msg);
+      },
+      error(msg) {
+        console.log("ERROR", msg);
+      },
+    },
+  };
+  return initJaegerTracer(config, options);
+};
+
+export default initTracer;
+
+
